6cde3d08920c84dc64554e66b1c56fae558dc914
fernando <fernando.oliveira@sofex.com.br>
2023-11-19 22:49:28 -0300

    C piscine C00 | OK 70%

---
+++ piscine-C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnascime <fnascime@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 16:36:47 by fnascime          #+#    #+#             */
+/*   Updated: 2023/09/01 20:15:37 by fnascime         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	letter;
+
+	letter = 122;
+	while (letter >= 97)
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
---
+++ piscine-C00/ex06/ft_print_comb2.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnascime <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/01 11:42:30 by fnascime          #+#    #+#             */
+/*   Updated: 2023/09/01 13:13:57 by fnascime         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_write_comb(int a, int b, int last)
+{
+	ft_putchar(48 + a / 10);
+	ft_putchar(48 + a % 10);
+	ft_putchar(' ');
+	ft_putchar(48 + b / 10);
+	ft_putchar(48 + b % 10);
+	if (last == 1)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+	int	last;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			last = !(a == 98 && b == 99);
+			ft_write_comb(a, b, last);
+			b++;
+		}
+		a++;
+	}
+}
